0c77b55fc1b7e1a71671f48c9d50cbbdccda3a47
stefka11 <sph_bg@abv.bg>
2025-01-18 11:55:31 +0200

    show home page

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');    //hbs ще е дефолтния енжин
 app.set('views', './src/views');  //указваме къде се намира папката views
 
-app.use(express.static('src/public')); //статичните файлове да са достъпни
-//app.use('/static', express.static('src/public')); //ако започва с /static – тогава го търси в src/public
+//app.use(express.static('src/public')); //статичните файлове да са достъпни
+app.use('/static', express.static('src/public')); //ако започва с /static – тогава го търси в src/public
 
 
 app.get ('/', (req, res) => {
-    res.send('It works!');
-    //res.render('home', {layout: false});
+    //res.send('It works!');
+    res.render('home', {layout: false});
 });
 
 app.listen(5001, ()=> console.log('Server is listening at http://localhost:5001...'));
