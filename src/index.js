cc3314fcb93b9e6f57b7c9ec7cdfb07bfc42a628
stefka11 <sph_bg@abv.bg>
2025-01-18 11:49:09 +0200

    setup express static

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import handlebars from 'express-handlebars';
 
 const app = express();
 
-//app.use(express.static('src/public')); //статичните файлове да са достъпни
-//app.use('/static', express.static('src/public')); //ако започва с /static – тогава го търси в src/public
-
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',   //задаваме да може да обработва с разширение hbs
 }));
 app.set('view engine', 'hbs');    //hbs ще е дефолтния енжин
 app.set('views', './src/views');  //указваме къде се намира папката views
+
+app.use(express.static('src/public')); //статичните файлове да са достъпни
+//app.use('/static', express.static('src/public')); //ако започва с /static – тогава го търси в src/public
+
 
 app.get ('/', (req, res) => {
     res.send('It works!');
