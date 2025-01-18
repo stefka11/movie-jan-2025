86c7cf32c34c4ff86c35105944900820f7b8dc4d
stefka11 <sph_bg@abv.bg>
2025-01-18 16:04:26 +0200

    add details page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 
 const movieController = Router();
+
 movieController.get('/create', (req, res) => {
     res.render('create');
+});
+
+movieController.get('/:movieId/details', (req, res) => {
+    res.render('details');
 });
 
 export default movieController;
