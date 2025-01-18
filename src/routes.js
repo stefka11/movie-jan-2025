f90c39b5664ea6b61e7c1d211d5acfb1cd0c3e8b
stefka11 <sph_bg@abv.bg>
2025-01-18 15:39:12 +0200

    change movies controller url

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import movieController from './controllers/movie-controller.js';
 const routes = Router();   //създаваме нов модуларен раут
 
 routes.use(homeController);  //Извикваме хом контролера, можем и други контролери по късно
-routes.use(movieController);
+routes.use('/movies', movieController);  //всичко започващо с /movies да извиква movieController
 
 routes.get ('*', (req, res) => {
     res.render('404');
