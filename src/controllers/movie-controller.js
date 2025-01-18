883419f52158871eb7de22a7947bf14362950fb1
stefka11 <sph_bg@abv.bg>
2025-01-18 15:22:50 +0200

    add create controller and page

---
+++ src/controllers/movie-controller.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const movieController = Router();
+movieController.get('/create', (req, res) => {
+    res.render('create');
+});
+
+export default movieController;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';  
 import homeController from './controllers/home-controller.js';
+import movieController from './controllers/movie-controller.js';
 
 const routes = Router();   //създаваме нов модуларен раут
 
 routes.use(homeController);  //Извикваме хом контролера, можем и други контролери по късно
+routes.use(movieController);
 
 routes.get ('*', (req, res) => {
     res.render('404');
