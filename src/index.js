35e554bf6b8f9eb87cf10c2817f7a5ea221922f5
stefka11 <sph_bg@abv.bg>
2025-01-18 09:48:17 +0200

    install and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Welcome");
+import express from 'express';
+
+const app = express();
+
+app.set('views', './src/views');  //указваме къде се намира папката views
+
+app.get ('/', (req, res) => {
+    res.send('It works!');
+    //res.render('home', {layout: false});
+});
+
+app.listen(5001, ()=> console.log('Server is listening at http://localhost:5001...'));
+
