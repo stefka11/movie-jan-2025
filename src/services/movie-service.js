4b4d0780e4b6ce6f8ed97ab7e62421a1398da19a
stefka11 <sph_bg@abv.bg>
2025-01-19 14:20:40 +0200

    add search by genre and year

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -8,6 +8,13 @@ export default {
         if (filter.search){
             result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()) );
         }
+
+        if (filter.genre){
+            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+        }
+        if (filter.year){
+            result = result.filter(movie => movie.year === filter.year);
+        }
         return result;
     },
 
